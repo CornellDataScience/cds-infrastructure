71450721312e046a1c959d44e508b9a8469ae878
Emily Wang <esw77@cornell.edu>
2022-11-18 18:02:55 -0500

    Trying to push helloworld to repo

--- scripts/sample-script/testScript.sh
+++ scripts/sample-script/testScript.sh
@@ -3,3 +3,6 @@ cd helloWorld
 echo "The command echo is like Linux's version of print()"
 echo "But if we tell echo to put its output into a text file like below..."
 echo "It shows up where we tell it to!" > helloWorld.txt
+git add helloWorld.txt
+git commit -m "Adding output of testScript.sh"
+git push
